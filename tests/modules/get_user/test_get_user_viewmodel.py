44b85bb58c147038517ea8c6c6756711a9b5911b
Vitor Soller <vgsoller@gmail.com>
2022-12-13 10:40:55 -0300

    add get_user_viewmodel.py with UT

---
+++ tests/modules/get_user/test_get_user_viewmodel.py
@@ -0,0 +1,22 @@
+from src.modules.get_user.get_user_viewmodel import GetUserViewmodel
+from src.shared.domain.entities.user import User
+from src.shared.domain.enums.state_enum import STATE
+
+
+class Test_GetUserViewModel:
+    def test_get_user_viewmodel(self):
+        user = User(
+            idUser=1,
+            name="Vitor Soller",
+            email="vitinho@hype.com",
+            state=STATE.APPROVED
+        )
+        userViewmodel = GetUserViewmodel(user=user).to_dict()
+
+        expected = {'idUser': 1,
+                    'name': 'Vitor Soller',
+                    'email': 'vitinho@hype.com',
+                1    'state': 'APPROVED',
+                    'message': 'the user was retrieved successfully'}
+
+        assert expected == userViewmodel
