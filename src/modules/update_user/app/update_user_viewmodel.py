6675375c56259e86f598d6dc488b20f76566e1a1
Vitor Soller <vgsoller@gmail.com>
2022-12-14 15:48:02 -0300

    add update_user_viewmodel.py with UT

---
+++ src/modules/update_user/app/update_user_viewmodel.py
@@ -0,0 +1,25 @@
+from src.shared.domain.entities.user import User
+from src.shared.domain.enums.state_enum import STATE
+
+
+class UpdateUserViewmodel:
+    idUser: int
+    name: str
+    email: str
+    state: STATE
+
+    def __init__(self, user: User):
+        self.idUser = user.idUser
+        self.name = user.name
+        self.email = user.email
+        self.state = user.state
+
+    def to_dict(self):
+        return {
+            'idUser': self.idUser,
+            'name': self.name,
+            'email': self.email,
+            'state': self.state.value,
+            'message': "the user was updated successfully"
+        }
+
