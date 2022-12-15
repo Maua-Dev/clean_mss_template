f3ae0526639fe805b0e9c71264e273a7a53cb821
Vitor Soller <vgsoller@gmail.com>
2022-12-15 11:43:26 -0300

    fix imports create_user_controller.py

--- src/modules/create_user/app/create_user_controller.py
+++ src/modules/create_user/app/create_user_controller.py
@@ -4,7 +4,6 @@ from .create_user_viewmodel import CreateUserViewmodel
 from src.shared.helpers.errors.controller_errors import MissingParameters, WrongTypeParameter
 from src.shared.helpers.errors.domain_errors import EntityError
 from src.shared.helpers.errors.usecase_errors import NoItemsFound
-from src.shared.helpers.external_interfaces.http_models import HttpRequest, HttpResponse
 from src.shared.helpers.external_interfaces.http_codes import OK, NotFound, BadRequest, InternalServerError, Created
 
 
