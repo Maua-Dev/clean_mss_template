3fcb2d4fa47c5dbc7ee3d03255a4668f47a99f7d
Vitor Soller <vgsoller@gmail.com>
2022-12-16 12:07:36 -0300

    user_repository_dynamo.py create_user

--- src/shared/infra/repositories/user_repository_dynamo.py
+++ src/shared/infra/repositories/user_repository_dynamo.py
@@ -1,10 +1,29 @@
+from decimal import Decimal
 from typing import List
 
 from src.shared.domain.entities.user import User
 from src.shared.domain.repositories.user_repository_interface import IUserRepository
+from src.shared.environments import Environments
+from src.shared.infra.dto.user_dynamo_dto import UserDynamoDto
+from src.shared.infra.external.dynamo.datasources.dynamo_datasource import DynamoDatasource
 
 
 class UserRepositoryDynamo(IUserRepository):
+
+    @staticmethod
+    def partition_key_format(idUser) -> str:
+        return f"user#{idUser}"
+
+    @staticmethod
+    def sort_key_format(idUser: int) -> str:
+        return f"#{idUser}"
+
+    def __init__(self):
+        self.dynamo = DynamoDatasource(endpoint_url=Environments.get_envs().endpoint_url,
+                                       dynamo_table_name=Environments.get_envs().dynamo_table_name,
+                                       region=Environments.get_envs().region,
+                                       partition_key=Environments.get_envs().dynamo_partition_key,
+                                       sort_key=Environments.get_envs().dynamo_sort_key)
     def get_user(self, idUser: int) -> User:
         pass
 
@@ -12,7 +31,12 @@ class UserRepositoryDynamo(IUserRepository):
         pass
 
     def create_user(self, new_user: User) -> User:
-        pass
+        new_user.idUser = self.get_user_counter()
+        user_dto = UserDynamoDto.from_entity(user=new_user)
+        resp = self.dynamo.put_item(partition_key=self.partition_key_format(new_user.idUser),
+                                    sort_key=self.sort_key_format(idUser=new_user.idUser), item=user_dto.to_dynamo(),
+                                    is_decimal=True)
+        return new_user
 
     def delete_user(self, idUser: int) -> User:
         pass
@@ -21,4 +45,11 @@ class UserRepositoryDynamo(IUserRepository):
         pass
 
     def get_user_counter(self) -> int:
-        pass
+
+        return self.update_counter()
+
+    def update_counter(self) -> int: #TODO fix this
+        counter = int(self.dynamo.get_item(partition_key='COUNTER', sort_key='COUNTER')['Item']['COUNTER'])
+        resp = self.dynamo.update_item(partition_key='COUNTER', sort_key='COUNTER', update_dict={'COUNTER': Decimal(counter+1)})
+
+        return int(resp['Attributes']['COUNTER'])
---
+++ tests/shared/infra/repositories/test_user_repository_dynamo.py
@@ -0,0 +1,18 @@
+import os
+
+import pytest
+
+from src.shared.infra.repositories.user_repository_dynamo import UserRepositoryDynamo
+from src.shared.infra.repositories.user_repository_mock import UserRepositoryMock
+
+
+class Test_UserRepositoryDynamo:
+    @pytest.mark.skip(reason="Needs dynamoDB")
+    def test_create_user(self):
+        os.environ["STAGE"] = "TEST"
+
+        user_repository = UserRepositoryDynamo()
+        user_repository_mock = UserRepositoryMock()
+        resp = user_repository.create_user(user_repository_mock.users[0])
+
+        assert True
